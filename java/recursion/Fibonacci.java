a4a154c142de25cc22897ba5dc83e291bb6784d7
varun sharma <varunsharma12@outlook.com>
2020-05-08 06:34:33 +0530

    Recursive implementation of fibonacci

--- java/recursion/Fibonacci.java
+++ java/recursion/Fibonacci.java
@@ -1,8 +1,29 @@
 package recursion;
 
+import java.util.Scanner;
+
 public class Fibonacci{
+
+
+    private static int recFib(int num){
+
+        if (num <= 1){
+
+            return num;
+        }
+
+        int n = recFib(num-1) + recFib(num-2);
+        return n;
+    }
 
     public static void main(String[] args) {
         
+        try(Scanner scan = new Scanner(System.in)){
+            
+            System.out.println("Please enter the number to calculate fibonacci series of the number.");
+            int num = scan.nextInt();
+            System.out.println("Read " + num);
+            System.out.println("Fibonacci number of " + num + " is " + recFib(num));
+        }
     }
 }

e1d0fb7eef2d65b70267f9a4e5a10b0e0a90b7b1
varun sharma <varunsharma12@outlook.com>
2020-05-27 07:31:51 +0530

    Tree modification to test symmetric property.

--- java/trees/binarytrees/symmetrictree/SymmetricTree.java
+++ java/trees/binarytrees/symmetrictree/SymmetricTree.java
@@ -22,11 +22,11 @@ public class SymmetricTree {
         System.out.println("Creating a tree.");
         Node root = new Node(1);
         root.left = new Node(2);
-        root.right = new Node(3);
+        root.right = new Node(2);
         root.left.left = new Node(4);
         root.left.right = new Node(5);
-        root.right.left = new Node(6);
-        root.right.right = new Node(7);
+        root.right.left = new Node(5);
+        root.right.right = new Node(4);
         System.out.println("Tree constructed.");
         boolean symmetric = tree.isSymmetric(root);
         System.out.println("is tree symmetric: " + symmetric);

e775f584155366ede2bae848b2f01b8d995ee349
varun sharma <varunsharma12@outlook.com>
2020-06-03 11:38:59 +0530

    Reverse only letters of a string solution using stack.

---
+++ java/strings/reverseonlyletters/ReverseOnlyLetters.java
@@ -0,0 +1,37 @@
+package strings.reverseonlyletters;
+
+import java.util.Stack;
+
+public class ReverseOnlyLetters {
+    
+
+    public String reverse(String data){
+
+        if(data == null || data.isEmpty()) return data;
+        
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < data.length(); i++){
+           if(Character.isLetter(data.charAt(i))) stack.add(data.charAt(i));
+        }
+
+        StringBuilder builder = new StringBuilder();
+        for(int i = 0; i < data.length(); i++){
+            if(Character.isLetter(data.charAt(i))){
+                builder.append(stack.pop());
+            }else{
+                builder.append(data.charAt(i));
+            }
+        }
+
+        return builder.toString();
+    }
+
+
+    public static void main(String[] args) {
+
+        ReverseOnlyLetters reverseOnlyLetters = new ReverseOnlyLetters();
+        String data = "a-jb-jan-van-rafd";
+        String reversed = reverseOnlyLetters.reverse(data);
+        System.out.println(reversed);
+    }
+}

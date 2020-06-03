2e1ec60ce6bdeff5af92348fc6082cb86799405a
varun sharma <varunsharma12@outlook.com>
2020-06-03 07:19:11 +0530

    mountain array using linear search.

---
+++ java/arrays/mountainarray/MountainArrayPeekIndexLinearSearch.java
@@ -0,0 +1,22 @@
+package arrays.mountainarray;
+
+public class MountainArrayPeekIndexLinearSearch {
+    
+    public int search(int[] data){
+        if(data == null || data.length == 0) return Integer.MIN_VALUE;
+        
+        for(int i = 0; i < data.length - 1; i++){
+            if(data[i] > data[i+1]) return data[i];
+        }
+        
+        return Integer.MIN_VALUE;
+    }
+
+    public static void main(String[] args) {
+
+        MountainArrayPeekIndexLinearSearch mountainArray = new MountainArrayPeekIndexLinearSearch();
+        int[] data = new int[]{0,2,1,0};
+        int element = mountainArray.search(data);
+        System.out.println("Mountain element " + element);
+    }
+}

19305514f290647b5eb70dc7a9406261bc00398c
varun sharma <varunsharma12@outlook.com>
2020-06-08 08:36:30 +0530

    Add numbers from two linked lists solution.

--- java/linkedlist/addnumbersfromtwolinkedlists/addnumbersfromtwolinkedlistsusingstrings/AddNumbersFromTwoLinkedLists.java
+++ java/linkedlist/addnumbersfromtwolinkedlists/addnumbersfromtwolinkedlistsusingstrings/AddNumbersFromTwoLinkedLists.java
@@ -1,5 +1,8 @@
 package linkedlist.addnumbersfromtwolinkedlists.addnumbersfromtwolinkedlistsusingstrings;
-
+//numbers in the list are in reverse order.
+//input: 2->4->3 5->6->4
+//Addition of two above numbers is 807
+//output: 7->0->8
 public class AddNumbersFromTwoLinkedLists {
     
     public Node addUsingStrings(Node node1, Node node2){

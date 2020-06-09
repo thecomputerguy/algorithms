96aa7549f264a06b493e3ec7d9e323273133e154
varun sharma <varunsharma12@outlook.com>
2020-06-09 11:23:25 +0530

    best time to buy and sell stocks solution.

--- kotlin/arrays/besttimetobuyandsellstocks/BestTimeToBuyAndSellStocks.kt
+++ kotlin/arrays/besttimetobuyandsellstocks/BestTimeToBuyAndSellStocks.kt
@@ -1,1 +1,22 @@
-fun main() = println("hello kotlin!")
+fun maxProfit(data : Array<Int>): Int{
+    var profit: Int = 0
+    var min: Int = Int.MAX_VALUE
+    
+    for (element in data) {
+        
+        if(element < min){
+            min = element
+        }else if(profit < element - min){
+            profit = element - min
+        }
+        
+    }
+
+    return profit
+}
+
+fun main(args: Array<String>) {
+    val data = arrayOf(7,1,5,3,6,4)
+    val result = maxProfit(data)
+    println("max profit is : $result")
+}

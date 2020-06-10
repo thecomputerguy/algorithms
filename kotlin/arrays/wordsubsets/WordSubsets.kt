41f8bf13ce25ff0d1bbdb7e6e5c5137b8e402085
varun sharma <varunsharma12@outlook.com>
2020-06-10 08:57:08 +0530

    fixed issue for repeated characters in B Array.

--- kotlin/arrays/wordsubsets/WordSubsets.kt
+++ kotlin/arrays/wordsubsets/WordSubsets.kt
@@ -1,46 +1,54 @@
-fun wordSubsets(input1: Array<String>, input2: Array<String>): List<String> {
+fun wordSubsets(A: Array<String>, B: Array<String>): List<String> {
     val result = mutableListOf<String>()
-    if (input1 == null || input1.isEmpty() || input2 == null || input2.isEmpty()) return result;
+    if (input1 == null || input1.isEmpty() || input2 == null || input2.isEmpty()) return result
 
-    val charCount: IntArray = IntArray(26)
-    buildCharCount(charCount, input2)
-
-    for (data in input1) {
-
-        val wordCharacterCount: IntArray = IntArray(26)
-        buildCharCount(wordCharacterCount, data)
-        var index: Int = 0;
-
-        for (value in charCount) {
-
-            val count: Int = wordCharacterCount[index]
-            if (value > count) break
-            index++
-        }
-
-        if (index == 26) {
-            result.add(data)
+    //build the max char count of B array first. These are number of characters that each word should have
+    //to be a universal word.
+    val maxCharCount: IntArray = IntArray(26)
+    for (word in B) {
+        val currentWordCount: IntArray = buildCharCount(word)
+        for (i in 0 until 26){
+            maxCharCount[i] = maxOf(currentWordCount[i], maxCharCount[i])
         }
     }
 
+    //iterate over A array and build the count of characters in each word
+    //if the count of characters in the word is less than the count of character in the max characters array
+    //then the word is not universal word as it does not have all the required characters.
+    for (word in A) {
 
-    return result;
+        val wordCharacterCount: IntArray = buildCharCount(word)
+        var valid: Boolean = true;
+
+        for (i in 0 until 26) {
+
+            val count: Int = wordCharacterCount[i]
+            val maxCount: Int = maxCharCount[i]
+            if (maxCount > count){
+                valid = false
+                break
+            }
+
+        }
+
+        if (valid) {
+            result.add(word)
+        }
+    }
+
+    return result
 }
 
-fun buildCharCount(mapping: IntArray, input1: String): Unit {
+fun buildCharCount(word: String): IntArray {
 
-    for (ch in input1) {
+    val mapping = IntArray(26)
+    for (ch in word) {
         val key: Int = ch - 'a'
         mapping[key] = mapping[key] + 1
     }
+    return mapping
 }
 
-fun buildCharCount(mapping: IntArray, input2: Array<String>): Unit {
-
-    for (word in input2) {
-        buildCharCount(mapping, word)
-    }
-}
 
 fun main(args: Array<String>) {
     println("word subsets problem solution.")

40989f7ed9f0f66dd5b51e6c4125884c001fa2f5
Varun Sharma <varunsharma12@outlook.com>
2020-06-25 19:29:07 +0530

    valid parenthesis problem solution.

---
+++ kotlin/strings/validparenthesis/ValidParenthesis.kt
@@ -0,0 +1,30 @@
+fun isValid(data: String): Boolean{
+    if(data == null || data.length == 0) return false
+    
+    var stack = java.util.Stack<Char>()
+    for (char in data) {
+        when(char){
+            '(', '[', '{' -> stack.push(char)
+            ')' -> if(!isValid(stack, '(')) return false
+            ']' -> if(!isValid(stack, '[')) return false
+            '}' -> if(!isValid(stack, '{')) return false
+            else -> return false
+        }
+    }
+    return true
+}
+
+fun isValid(stack: java.util.Stack<Char>, char: Char): Boolean{
+    if(stack.isEmpty())  return false
+    if(stack.pop() == char){
+        return true
+    }
+    return false
+}
+
+fun main(args: Array<String>) {
+   val result1 = isValid("(){}[]")
+   println("is valid? $result1")
+   val result2 = isValid("()][{}")
+   println("is valid? $result2")
+}

845536ce60eaedb4a4a5c069c82033bbb67b77ff
Varun Sharma <varunsharma12@outlook.com>
2020-07-05 13:33:08 +0530

    Longest substring with no repeating characters solution in java.

---
+++ java/slidingwindow/longestsubstringwithnorepeatingcharacters/LongestSubstringWithNoRepeatingCharacter.java
@@ -0,0 +1,27 @@
+package slidingwindow.longestsubstringwithnorepeatingcharacters;
+
+import java.util.*;
+
+public class LongestSubstringWithNoRepeatingCharacter {
+
+    public static int findLength(String str) {
+        int winStart = 0;
+        int maxLength = Integer.MIN_VALUE;
+        Map<Character, Integer> holder = new HashMap<>();
+        for (int winEnd = 0; winEnd < str.length(); winEnd++) {
+
+            if (holder.containsKey(str.charAt(winEnd))) {
+                winStart = Math.max(winStart, holder.get(str.charAt(winEnd)) + 1);
+            }
+            holder.put(str.charAt(winEnd), winEnd);
+            maxLength = Math.max(maxLength, winEnd - winStart + 1);
+        }
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        String S = "abccde";
+        int result = findLength(S);
+        System.out.println(result);
+    }
+}

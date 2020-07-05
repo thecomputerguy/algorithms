043e3880c42188d0aacc20547b2ed769ca47e035
Varun Sharma <varunsharma12@outlook.com>
2020-07-05 12:03:57 +0530

    Fruits in the basket problem solution in java.

---
+++ java/slidingwindow/fruitsinthebasket/FruitsInTheBasket.java
@@ -0,0 +1,34 @@
+package slidingwindow.fruitsinthebasket;
+
+import java.util.*;
+
+class FruitsInTheBasket {
+
+    public static int findLength(char[] arr) {
+        int maxFruits = Integer.MIN_VALUE;
+        int winStart = 0;
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        for (int winEnd = 0; winEnd < arr.length; winEnd++) {
+            char fruit = arr[winEnd];
+            map.put(fruit, map.getOrDefault(fruit, 0) + 1);
+
+            while (map.size() > 2) {
+                char charAtWinStart = arr[winStart];
+                map.put(charAtWinStart, map.get(charAtWinStart) - 1);
+                if (map.get(charAtWinStart) == 0) {
+                    map.remove(charAtWinStart);
+                }
+                winStart++;
+            }
+            maxFruits = Math.max(maxFruits, winEnd - winStart + 1);
+        }
+        return maxFruits;
+    }
+
+    public static void main(String[] args) {
+        char[] fruits = new char[] { 'A', 'B', 'C', 'B', 'B', 'C' };
+        int result = findLength(fruits);
+        System.out.println(result);
+    }
+
+}

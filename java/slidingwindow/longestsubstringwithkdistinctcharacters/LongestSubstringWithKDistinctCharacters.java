734c9ff008ec87318629a647b18c38f2a40f79f9
Varun Sharma <varunsharma12@outlook.com>
2020-07-05 08:26:11 +0530

    longest substring with k distinct characters solution in java.

---
+++ java/slidingwindow/longestsubstringwithkdistinctcharacters/LongestSubstringWithKDistinctCharacters.java
@@ -0,0 +1,49 @@
+package slidingwindow.longestsubstringwithkdistinctcharacters;
+
+import java.util.*;
+
+public class LongestSubstringWithKDistinctCharacters {
+
+    public static int findLength(String str, int k) {
+        int winStart = 0;
+        int maxLength = Integer.MIN_VALUE;
+        int uniqueChars = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int winEnd = 0; winEnd < str.length(); winEnd++) {
+
+            char charAtWinEnd = str.charAt(winEnd);
+
+            if (map.containsKey(charAtWinEnd)) {
+                map.put(charAtWinEnd, map.get(charAtWinEnd) + 1);
+            } else {
+                uniqueChars++;
+                map.put(charAtWinEnd, 1);
+            }
+
+            while (uniqueChars > k) {
+                uniqueChars--;
+                char charAtWinStart = str.charAt(winStart);
+                if (map.get(charAtWinStart) > 1) {
+                    map.put(charAtWinStart, map.get(charAtWinStart) - 1);
+                } else if (map.get(charAtWinStart) == 1) {
+                    map.remove(charAtWinStart);
+                }
+                winStart++;
+            }
+            maxLength = Math.max(maxLength, winEnd - winStart + 1);
+        }
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        int k = 3;
+        String str = "cbbebi";
+        int result = findLength(str, k);
+        System.out.println(result);
+
+        int k1 = 1;
+        String str1 = "araaci";
+        int result1 = findLength(str1, k1);
+        System.out.println(result1);
+    }
+}

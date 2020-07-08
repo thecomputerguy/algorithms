1fef4ddc3b0d7a4215eb8c5cc7f56aa505812d12
Varun Sharma <varunsharma12@outlook.com>
2020-07-08 18:22:22 +0530

    😉: Middle of the linked list linear time and constant space solution in java.

---
+++ java/linkedlist/middleofthelinkedlist/MiddleOfTheLinkedList.java
@@ -0,0 +1,41 @@
+package linkedlist.middleofthelinkedlist;
+
+public class MiddleOfTheLinkedList {
+
+    public static ListNode findMiddle(ListNode head) {
+        if (head == null)
+            return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        System.out.println("Middle Node: " + findMiddle(head).value);
+
+        head.next.next.next.next.next = new ListNode(6);
+        System.out.println("Middle Node: " + findMiddle(head).value);
+
+        head.next.next.next.next.next.next = new ListNode(7);
+        System.out.println("Middle Node: " + findMiddle(head).value);
+    }
+
+}
+
+class ListNode {
+    int value = 0;
+    ListNode next;
+
+    ListNode(int value) {
+        this.value = value;
+    }
+}
